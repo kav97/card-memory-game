a7033060ab4b37ddb017a726a51b7b1498b9d29d
Kavandeep <kavan1@live.co.uk>
2022-07-09 20:47:57 +0100

    added array file and linked to main script

---
+++ data/pokemon.js
@@ -0,0 +1,58 @@
+export const pokemonArray = [
+    {
+        id: 3,
+        name: "venusaur",
+        types: ["grass", "poison"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+    },
+    {
+        id: 6,
+        name: "charizard",
+        types: ["fire", "flying"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+    },
+    {
+        id: 9,
+        name: "blastoise",
+        types: ["water"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+    },
+    {
+        id: 18,
+        name: "pidgeot",
+        types: ["normal", "flying"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png",
+    },
+    {
+        id: 25,
+        name: "pikachu",
+        types: ["electric"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    },
+    {
+        id: 144,
+        name: "articuno",
+        types: ["ice", "flying"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/144.png",
+    },
+    {
+        id: 145,
+        name: "zapdos",
+        types: ["electric", "flying"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/145.png",
+    },
+    {
+        id: 146,
+        name: "moltres",
+        types: ["fire", "flying"],
+        sprite:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/146.png",
+    },
+]
---
+++ src/script.js
@@ -0,0 +1,1 @@
+import pokemonArray from "../data/pokemon.js";

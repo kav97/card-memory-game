a5e73d163d3e027d3982c9bcff9d3d7289394427
Kavandeep <kavan1@live.co.uk>
2022-07-12 18:38:09 +0100

    added function that shuffles array

--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
 import { pokemonArray } from "../data/pokemon.js";
 
 // query selectors - for all aspects that need updates
-// global variables
-// functions - DRY, does 1 thing, meaningful names
-// event listeners - listen out for changes to page and updates using code.
-
 const container = document.querySelector(".board__container");
+const pokemonCard = document.querySelectorAll(".card");
+console.log(pokemonCard);
+// global variables
+
+// functions - DRY, does 1 thing, meaningful names
+//      cards to flip when clicked
+//      button/ click on card to start game
+//      reset button to start game again, with newly shuffled array
+//      mopves to be recorded and time recorded
+
+
+const shuffleArray = (array) => {
+    return [...array].sort(() => Math.random() - 0.5);
+}
 
 const addPokemon = () => {
     pokemonArray.forEach(pokemon => {
@@ -19,4 +29,5 @@ const addPokemon = () => {
     });
 };
 
+// event listeners - listen out for changes to page and updates using code.
 container.addEventListener("load", addPokemon());
